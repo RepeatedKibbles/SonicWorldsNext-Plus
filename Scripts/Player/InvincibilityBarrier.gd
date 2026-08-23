9566edd03f9d3d417a516fb20d32ec3bfb309264
QuartzTS <marawanessamreda456@gmail.com>
2026-08-23 17:52:34 +0300

    Another hotfix, I guess?
    I hope everything is okay, now..

--- Scripts/Player/InvincibilityBarrier.gd
+++ Scripts/Player/InvincibilityBarrier.gd
@@ -3,19 +3,13 @@ extends Node2D
 
 ## An array of values used to offset the rotation angle of the stars for each pair
 const STAR_ANGLE_OFFSETS: Array[float] = [0, 118.125, 0, 81.5625]
+## The number of stars
+const STAR_NUM: int = 8
 ## The radius of rotation around the player
 const STAR_ROTATION_RADIUS: int = 16
 ## The length of the circular queue used to store the player's position
 const POSITION_QUEUE_LENGTH: int = 12
-
-## An array of arrays that store the frames to cycle through
-var star_frame_arr: Array[PackedByteArray] = [
-	PackedByteArray([7, 4, 6, 4, 4, 6, 4, 7, 4, 6, 6, 4]),
-	PackedByteArray([2, 3, 4, 5, 6, 7, 6, 5, 4, 3]),
-	PackedByteArray([1, 2, 3, 4, 5, 6, 7, 6, 5, 4, 3, 2]),
-	PackedByteArray([0, 1, 2, 3, 4, 5, 6, 5, 4, 3, 2, 1]),
-]
-var regions: Array[Rect2] = [
+const REGIONS: Array[Rect2] = [
 	Rect2(0, 0, 31, 31),
 	Rect2(31, 0, 31, 31),
 	Rect2(62, 0, 31, 31),
@@ -24,6 +18,14 @@ var regions: Array[Rect2] = [
 	Rect2(155, 0, 31, 31),
 	Rect2(186, 0, 31, 31),
 	Rect2(217, 0, 31, 31),
+]
+
+## An array of arrays that store the frames to cycle through
+var star_frame_arr: Array[PackedByteArray] = [
+	PackedByteArray([7, 4, 6, 4, 4, 6, 4, 7, 4, 6, 6, 4]),
+	PackedByteArray([2, 3, 4, 5, 6, 7, 6, 5, 4, 3]),
+	PackedByteArray([1, 2, 3, 4, 5, 6, 7, 6, 5, 4, 3, 2]),
+	PackedByteArray([0, 1, 2, 3, 4, 5, 6, 5, 4, 3, 2, 1]),
 ]
 ## A circular queue that stores the player's position
 var player_position_queue: PackedVector2Array
@@ -51,7 +53,7 @@ var stars: Array[Sprite2D]
 func _ready() -> void:
 	# Resize some arrays
 	player_position_queue.resize(POSITION_QUEUE_LENGTH)
-	star_position_arr.resize(4)
+	star_position_arr.resize(ceili(STAR_NUM))
 	star_angle.resize(2)
 	star_frame.resize(2)
 	# Append the child nodes that represent the stars of this barrier instance to the stars array
@@ -60,13 +62,8 @@ func _ready() -> void:
 
 
 func _physics_process(delta: float) -> void:
-	# If not visible, then only store the player's position
-	# This should make the stars not glitch out when destroying a second invincibility monitor
+	# Don't do anything AT ALL if it ain't visible
 	if not visible:
-		for i: int in player_position_queue.size():
-			player_position_queue[i] = player.centerReference.global_position
-		for i: int in star_position_arr.size():
-			star_position_arr[i] = player.centerReference.global_position
 		return
 	
 	# Increment the frame indices
@@ -95,58 +92,16 @@ func _physics_process(delta: float) -> void:
 	star_angle[0] = fmod(star_angle[0] + 101.25 * player.direction * delta * 60, 360) 
 	star_angle[1] = fmod(star_angle[1] + 11.25 * player.direction * delta * 60, 360)
 	
+	# Draw the stars every frame
+	queue_redraw()
+
+
+func _draw() -> void:
 	# Iterate over the stars to position and animate em
 	# Also, there are local variables that are defined here to keep the code nice and clean,
 	# and avoid repeating pieces of code
 	# (and also cuz nobody likes scrolling horizontally lol..)
-	#for star: Sprite2D in stars:
-		## The index of the star node in the array
-		#var star_index: int = stars.find(star)
-		## The current pair number
-		#var star_pair_num: int = floori(star_index / 2)
-		## Is the index number even or not?
-		#var is_index_even: bool = star_index % 2 == 0
-		## The array that's used to animate the current pair
-		#var frame_arr: PackedByteArray = star_frame_arr[star_pair_num]
-		## The index that points to a variation of a frame
-		## Here, it points to the second pair if the index is 1,
-		## since it's the one whose stars are animated with the frame array of the least amount of frames,
-		## it points to the other pairs if it's zero
-		#var frame_index: int = int(star_pair_num == 1)
-		## The increment of the frame
-		## It just increments the frames of the star, whose index is even, by half the amount of frames
-		## of the current frame array, eg. If the array has 12 frame numbers,
-		## then increase the current frame number by 6
-		## Also, the main pair is the exception here, we don't wanna increments the frames of the stars there..
-		#var frame_increment: int = frame_arr.size() * int(is_index_even and star_index > 0) / 2
-		## The index that points to a variation of an angle
-		## Here, it points to the main pair if it's 0, and to the trail pair if it's 1,
-		## since the main pair should be rotated faster than the trail pairs
-		#var angle_index: int = int(star_pair_num != 0)
-		## The increment of the angle
-		## It just increments the star by 180 if its index is even, and offsets the rotation of the star
-		## by the angle offset respective to the current pair
-		#var angle_increment: float = 180 * int(is_index_even) + STAR_ANGLE_OFFSETS[star_pair_num]
-		## The rotation of the star
-		## It's set to the angle value plus the increment
-		## And since we were dealing with the angles in degrees, convert em to radians
-		#var star_rotation: float = deg_to_rad(star_angle[angle_index] + angle_increment)
-		## The position of the star relative to the player
-		## It just rotates the star around the center, multiplied by the radius to offset it away
-		#var star_relative_position: Vector2 = Vector2.from_angle(star_rotation) * STAR_ROTATION_RADIUS
-		## Set the star position using the position stored in the array respective to the current pair,
-		## along with the relative position to rotate it
-		#star.global_position = star_position_arr[star_pair_num] + star_relative_position
-		## And finally, animate the star using the frame number for the current frame array plus the increment
-		#star.frame = frame_arr[(star_frame[frame_index] + frame_increment) % frame_arr.size()]
-	queue_redraw()
-
-
-# This is just an experiment to draw the stars without Sprite2D, but not sure if there are better solutions..
-func _draw() -> void:
-	for star_index in 8:
-		# The index of the star node in the array
-		#var star_index: int = stars.find(star)
+	for star_index in STAR_NUM:
 		# The current pair number
 		var star_pair_num: int = floori(star_index / 2)
 		# Is the index number even or not?
@@ -179,16 +134,11 @@ func _draw() -> void:
 		# The position of the star relative to the player
 		# It just rotates the star around the center, multiplied by the radius to offset it away
 		var star_relative_position: Vector2 = Vector2.from_angle(star_rotation) * STAR_ROTATION_RADIUS
-		# Set the star position
-		#star.global_position = star_relative_position + star_position_arr[star_pair_num]
-		# Animate the star
-		#star.frame = frame_arr[(star_frame[frame_index] + frame_increment) % frame_arr.size()]
-		# Draw the star? (The main drawback for this method is that it needs the position to be in Rect2 and
-		# in local space, and it also requires the frames of the star to be specified in region rects,
-		# and currently, it's still pretty janky..)
+		# Draw the star in set position and region
 		draw_texture_rect_region(
-			star_spritesheet,
-			Rect2(to_local(star_relative_position + 
-			star_position_arr[star_pair_num] - Vector2(16, 16)), Vector2(31, 31)),
-			regions[frame_arr[(star_frame[frame_index] + frame_increment) % frame_arr.size()]]
+			star_spritesheet, # The full spritesheet
+			Rect2( # The position and size in a rect
+				to_local(star_relative_position + star_position_arr[star_pair_num] - Vector2(16, 16)),
+				Vector2(31, 31)),
+			REGIONS[frame_arr[(star_frame[frame_index] + frame_increment) % frame_arr.size()]] # The drawn region
 		)
